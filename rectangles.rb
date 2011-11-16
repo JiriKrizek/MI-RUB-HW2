7d7701326097a208ea7c83c6fae06654810818c3
Jiri Krizek <jkrizek@gmail.com>
2011-11-16 07:13:08 +0100

    Nacitani informaci

--- rectangles.rb
+++ rectangles.rb
@@ -3,7 +3,25 @@ def overlap?
 end
 
 def input
-	puts "Zadejte delku hrany prvniho ctverce: "
+	print "Zadejte delku hrany prvniho ctverce: "
+  a1 = gets
+
+  print "Zadejte x-ovou souradnici stredu prvniho ctverce: "
+  x1 = gets
+
+  print "Zadejte y-ovou souradnici stredu prvniho ctverce: "
+  y1 = gets
+
+  print "Zadejte delku hrany druheho ctverce: "
+  a2 = gets
+
+  print "Zadejte x-ovou souradnici stredu druheho ctverce: "
+  x2 = gets
+
+  print "Zadejte y-ovou souradnici stredu druheho ctverce: "
+  y2 = gets
+  
+  print "Obsah sjednoceni dvou ctvercu je ."
 end
 
 input
