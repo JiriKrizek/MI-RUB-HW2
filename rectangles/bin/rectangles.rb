f2eb6258c4040a8fa2670938264e320d685994b2
Jiri Krizek <jkrizek@gmail.com>
2012-02-05 21:27:35 +0100

    Parsovani vstupu

--- rectangles/bin/rectangles.rb
+++ rectangles/bin/rectangles.rb
@@ -1,27 +1,69 @@
-def overlap?
+class Rectangle
 
+    attr_reader :a, :x, :y
+
+    def initialize(a,x,y)
+      @a = Float(a)
+      @x = Float(x)
+      @y = Float(y)
+    end
+
+    def overlap?(rect)
+      false unless rect.is_a? Rectangle
+    end
 end
 
-def input
-	print "Zadejte delku hrany prvniho ctverce: "
-  a1 = gets
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
 
-  print "Zadejte x-ovou souradnici stredu prvniho ctverce: "
-  x1 = gets
-
-  print "Zadejte y-ovou souradnici stredu prvniho ctverce: "
-  y1 = gets
-
-  print "Zadejte delku hrany druheho ctverce: "
-  a2 = gets
-
-  print "Zadejte x-ovou souradnici stredu druheho ctverce: "
-  x2 = gets
-
-  print "Zadejte y-ovou souradnici stredu druheho ctverce: "
-  y2 = gets
-  
-  print "Obsah sjednoceni dvou ctvercu je ."
+  def is_non_negative_number?
+    if is_number? and Float(self)>=0
+      true
+    else
+      false
+    end
+  end
 end
 
-input
+class Main
+  INPUT_ERROR="Spatny vstup."
+
+  def overlap?
+    raise NotImplementedError
+  end
+
+  def input
+    r1 = inputRect("prvniho")
+    r2 = inputRect("druheho")
+    [r1, r2]
+  end
+
+private
+  def inputRect(text)
+  	print "Zadejte delku hrany #{text} ctverce: "
+    a = gets
+    input_error unless a.is_non_negative_number?
+
+    print "Zadejte x-ovou souradnici stredu #{text} ctverce: "
+    x = gets
+    input_error unless x.is_number?
+
+    print "Zadejte y-ovou souradnici stredu #{text} ctverce: "
+    y = gets
+    input_error unless y.is_number?
+
+    rect = Rectangle.new(a,x,y)
+#    print "Obsah sjednoceni dvou ctvercu je ."
+  end
+
+  def input_error
+    puts INPUT_ERROR
+    exit
+  end
+end
+
+m = Main.new
+Array rect = m.input
+p rect[0].overlap?(rect[1])
