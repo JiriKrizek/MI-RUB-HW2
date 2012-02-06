5eeaec34c1e5674319dc0662c971c1c6f0006c3c
Jiri Krizek <jkrizek@gmail.com>
2012-02-06 16:22:50 +0100

    Zadani

--- rectangles/bin/main.rb
+++ rectangles/bin/main.rb
@@ -1,3 +1,12 @@
+# Vytvořte aplikaci, která spočítá obsah dvou čtverců sjednocených do jednoho obrazce.
+
+# Oba čtverce se zadávají pomocí souřadnic geometrického středu a délky hrany (používáme 
+# bezrozměrnou veličinu). Pokud je zadán vstup, který není číslem, aplikace vypíše 
+# „Spatny vstup.“ a okamžitě se ukončí. Stejným způsobem reaguje, když je zadána záporná 
+# délka hrany. Nulová délka hrany aplikaci nevadí. Pokud ctverce nemají společný ani jeden 
+# bod (např. když se nedotýkají ani jedním rohem), aplikace na to zareaguje textem 
+# „Ctverce se ani nedotykaji.“
+
 require '../lib/main.rb'
 
 m = Main.new
