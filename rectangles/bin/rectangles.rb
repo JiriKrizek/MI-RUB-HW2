29f63e4ef9d9e62f4636a3bdf867fcb4f26221cc
Jiri Krizek <jkrizek@gmail.com>
2012-02-06 01:23:32 +0100

    Pocitame

--- rectangles/bin/rectangles.rb
+++ rectangles/bin/rectangles.rb
@@ -1,15 +1,21 @@
 class Rectangle
-
-    attr_reader :a, :x, :y
+    attr_reader :a, :x1, :x2, :y1, :y2
 
     def initialize(a,x,y)
       @a = Float(a)
-      @x = Float(x)
-      @y = Float(y)
+      @x1 = Float(x)-(@a/2)
+      @y1 = Float(y)-(@a/2)
+      @x2 = x1+@a
+      @y2 = y1+@a
     end
 
     def overlap?(rect)
-      false unless rect.is_a? Rectangle
+      return false unless rect.is_a? Rectangle
+
+      return (@x1 <= rect.x2 && 
+              @x2 >= rect.x1 && 
+              @y1 <= rect.y2 && 
+              @y2 >= rect.y1)
     end
 end
 
@@ -29,10 +35,6 @@ end
 
 class Main
   INPUT_ERROR="Spatny vstup."
-
-  def overlap?
-    raise NotImplementedError
-  end
 
   def input
     r1 = inputRect("prvniho")
@@ -66,4 +68,22 @@ end
 
 m = Main.new
 Array rect = m.input
-p rect[0].overlap?(rect[1])
+r1=rect[0]
+r2=rect[1]
+
+if !r1.overlap?(r2)
+  print "Ctverce se ani nedotykaji."
+  exit
+end
+
+p r2
+p r1
+x_overlap = ([r2.x2,r1.x2].min) - ([r2.x1, r1.x1].max)
+p x_overlap
+y_overlap = ([r2.y2,r1.y2].min) - ([r2.y1, r1.y1].max)
+p y_overlap
+overlap = x_overlap * y_overlap
+p overlap
+
+sum = r1.a*r1.a + r2.a*r2.a - overlap
+p sum
